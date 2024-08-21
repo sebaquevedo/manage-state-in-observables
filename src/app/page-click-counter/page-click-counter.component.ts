fee42a51da6912ecbad8f3021e8ccefd36d26e7f
Sebastianm <sebaqd91@gmail.com>
2024-08-21 18:09:47 +0100

    scan and merge

--- src/app/page-click-counter/page-click-counter.component.ts
+++ src/app/page-click-counter/page-click-counter.component.ts
@@ -1,36 +1,45 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { fromEvent } from 'rxjs';
+import { fromEvent, map, merge, of, scan, Subject } from 'rxjs';
 import { AsyncPipe, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-page-click-counter',
   standalone: true,
   imports: [AsyncPipe, NgStyle, MatButtonModule],
-  template: `
-    <section class="app-description">
+  template: ` <section class="app-description">
       <div>
         <h1>Page Click Visualizer</h1>
         <p>Start clicking around the page...</p>
       </div>
     </section>
     <div>
-      <button (click)="clickEvents = []; $event.stopPropagation()" mat-stroked-button>Reset State</button>
+      <button
+        (click)="reset$.next(); $event.stopPropagation()"
+        mat-stroked-button
+      >
+        Reset State
+      </button>
     </div>
-    @for (click of clickEvents; track $index) {
-      <div [style.left.px]="click.clientX"
-           [style.top.px]="click.clientY" class="click"></div>
-    }`
+    @for (click of clicks$ | async; track $index) {
+    <div
+      [style.left.px]="click.clientX"
+      [style.top.px]="click.clientY"
+      class="click"
+    ></div>
+    }`,
 })
 export class PageClickCounterComponent {
-  
-  clicks$ = fromEvent<PointerEvent>(document, 'click')
-    .subscribe(
-      event => {
-        this.clickEvents.push(event)
-      }
-    )
-  
-  clickEvents: PointerEvent[] = [];
-
+  reset$ = new Subject<void>();
+  clicks$ = merge(
+    fromEvent<PointerEvent>(document, 'click').pipe(map(accumulationHandler)),
+    this.reset$.pipe(map(resetHandler))
+  ).pipe(
+    scan((state: PointerEvent[], stateHandlerFn) => stateHandlerFn(state), [])
+  );
 }
+
+const accumulationHandler = (event: PointerEvent) => (state: PointerEvent[]) =>
+  [...state, event];
+
+const resetHandler = (event: void) => (state: PointerEvent[]) => [];
